c427f3408e52c5063326f2e2d0d52cc3b3c77f64
DimasVeliz <trevor90094@yandex.com>
2023-04-21 16:03:35 +0200

    navbar in place with logo, TODO:remake to have rounded

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import Logo from "../img/logo.png";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className="container">
+        <div className="logo">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="links">
+          <Link className='link' to="/?cat=bash">
+            <h6>Bash</h6>
+          </Link>
+          <Link className='link' to="/?cat=cpp"><
+            h6>CPP</h6>
+          </Link>
+          <Link className='link' to="/?cat=python">
+            <h6>Python</h6>
+          </Link>
+          <Link className='link' to="/?cat=dotnet">
+            <h6>Dotnet</h6>
+          </Link>
+          <Link className='link' to="/?cat=code">
+            <h6>Code</h6>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
