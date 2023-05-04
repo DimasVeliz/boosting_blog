757499fbdc0a3fc698511f54617d250e162117e7
DimasVeliz <trevor90094@yandex.com>
2023-05-04 23:40:43 +0200

    loging page working with api call, miss-using the sameSite cookie, important

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,41 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import api from '../service/api';
 
 const Login = () => {
+
+  const navigate = useNavigate();
+
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+  const [err, setError] = useState(null);
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await api.login(inputs).then(response => console.log(response.data));
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+
+
   return (
     <div className='auth'>
       <h1>Login</h1>
       <form >
-        <input required type='text' placeholder='email'/>
-        <input required type="password" placeholder='password' />
-        <button>Login</button>
-        <p>There was an error</p>
+        <input required type='text' placeholder='email' name='email' onChange={handleChange}/>
+        <input required type="password" placeholder='password' name='password' onChange={handleChange} />
+        <button onClick={handleSubmit}>Login</button>
+        {err && <p>{err}</p>}
         <span>Don't you have an account? <Link to="/register">Register</Link> </span>
       </form>
     </div>
--- src/service/api.js
+++ src/service/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
 
-const api= {
+const api = {
     register: (registerDto) =>
-        axios.post("v1/auth/register",registerDto)
+        axios.post("v1/auth/register", registerDto),
+    login: (authenticateDto) =>
+        axios.post("v1/auth/authenticate", authenticateDto)
 }
 
 export default api;
